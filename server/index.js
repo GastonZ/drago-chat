c079cda49ddb8c17fb69e7a6151c560d18565c6c
GastonZ <gastonzappullaryu@gmail.com>
2023-01-04 16:32:47 -0300

    setting up server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const morgan = require('morgan')
-const {Server} = require('socket.io')
-const http = require('http')
-const cors = require('cors')
+import express from 'express'
+import morgan from 'morgan'
+import {Server as SocketServer} from 'socket.io'
+import http from 'http'
+import cors from 'cors'
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new SocketServer(server)
 
 app.use(cors())
-app.use(morgan('dev'))
+app.use(morgan("dev"))
+
 app.listen(3000)
 console.log('server started on port 3000');
