23d66c6e6d73db68d59afe26daad5ad3a825ce12
GastonZ <gastonzappullaryu@gmail.com>
2023-01-05 19:02:24 -0300

    Listo v1

--- server/index.js
+++ server/index.js
@@ -3,15 +3,23 @@ import morgan from 'morgan'
 import {Server as SocketServer} from 'socket.io'
 import http from 'http'
 import cors from 'cors'
+import {dirname, join} from 'path'
+import {fileURLToPath} from 'url'
+
 import {PORT} from './config.js'
 
+
+
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname);
 const server = http.createServer(app)
 const io = new SocketServer(server, {
     cors: {
         origin: 'http://localhost:3000'
     }
 })
+
 
 app.use(cors())
 app.use(morgan("dev"))
@@ -24,6 +32,9 @@ io.on('connection', (socket)=> {
         })
     })
 })
+
+app.use(express.static(join(__dirname, '../client/build')))
+
 
 server.listen(PORT)
 console.log('server started on port ' + PORT);
